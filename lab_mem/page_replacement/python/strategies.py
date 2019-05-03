49dc9baf4fdef465ea8881dd18b7a7a497a19fc8
David Ferreira Quaresma <david.quaresma@ccc.ufcg.edu.br>
2019-05-03 11:51:40 -0300

    Add initial implementation of second-chance

--- lab_mem/page_replacement/python/strategies.py
+++ lab_mem/page_replacement/python/strategies.py
@@ -16,8 +16,34 @@ class Fifo:
   def access(self, frameId, isWrite):
     pass
 
+class SecondChance(Fifo):
+    def __init__(self, algorithm):
+      super().__init__(algorithm)
+      
+    def put(self, frameId, bit=0):
+      super().put((frameId, bit))
+
+    def evict(self):
+      while not super().fila.empty():
+        (frameId, bit) = super().evict()
+        if bit == 1:
+          self.put(frameId)
+        else:
+          return frameId
+      return None
+
+    def access(self, frameId, isWrite):
+      for i in range(super().fila.size()):
+        (frame, bit) = super().evict()
+        if frame == frameId:
+          self.put(frameId, 1)
+        else:
+          self.put(frameId, bit)
+      
 def get_strategy(algorithm):
     if algorithm == "fifo":
         return Fifo(algorithm)
+    elif algorithm == "second-chance":
+        return SecondChance(algorithm)
     else:
         raise Exception(algorithm + " strategy not implemented")
